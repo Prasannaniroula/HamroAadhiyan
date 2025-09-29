5ef5a3780bd6e3c621d2f9f3b1cb143a9ca70e06
Prasannaniroula <Prasannaniroula5@gmail.com>
2025-09-29 21:44:44 +0545

    feat: implement login functionality with validation and JWT token generation

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -39,3 +39,35 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
+
+export const loginUser = async (req, res) => { 
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.status(400).json({message: "Please fill all the fields"})
+       }
+    try {
+        // Check if user exists
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        // Compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        // Create and assign a token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        });
+
+        res.status(200).json({ message: "Logged in successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: "Server error" });
+    } 
+}
