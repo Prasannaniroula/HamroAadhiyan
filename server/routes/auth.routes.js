b57f3c2c373a94eaa6c397c1d32ec6d0ccc707d6
Prasannaniroula <Prasannaniroula5@gmail.com>
2026-07-05 12:55:46 +0545

    Solved the login error that was occuring in the loggedin state even after logout

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -34,7 +34,7 @@ const issueSocialJwtAndCookie = (req, res) => {
   res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none", 
+      sameSite: "lax", 
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
   });
   
